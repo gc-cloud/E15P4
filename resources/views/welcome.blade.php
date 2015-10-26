ebb9d67bee09a2f7df70e11c0da5fa4f52ad928c
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-10-25 21:43:24 -0500

    Basic theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
 
-    <title>Zudbu: A Healthy body, mind and spirit</title>
+    <title>Zudbu</title>
 
     <!-- Bootstrap core CSS -->
     <link href="dist/css/bootstrap.min.css" rel="stylesheet">
@@ -23,14 +23,17 @@
 
     <!-- Laravel nice font and app specific CSS -->
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <link href="/css/app.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Ubuntu+Condensed' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Cuprum' rel='stylesheet' type='text/css'>
+    <!-- <link href="/css/app.css" rel="stylesheet" type="text/css"> THIS IS THE GULP CSS-->
+    <link href="/css/zudbu.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
   </head>
 
   <body>
@@ -44,7 +47,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">Zudbu Home</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <form class="navbar-form navbar-right">
@@ -63,63 +66,44 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Place holder for P4 - Something Awesome!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
-      </div>
-    </div>
-
-
-    <div class="container content row">
-        <div class="title jumbotron">Laravel 5</div>
-        <h1> Place holder for P4 - Something Awesome!</h1>
-        <div class="form-group">
-        {!! Form::open(array('url' => '/logs')) !!}
-          <p>This is a form!</p>
-          {!!Form::password('password', array('class' => 'awesome'))!!} Password<br>
-          {!!Form::radio('something', 'value')!!} Pick something<br>
-          {!!Form::radio('something', 'value')!!} Pick something else<br>
-          {!!Form::submit('Get logs!', array('class' => 'btn btn-primary'))!!}
+        <h1>Zudbu</h1>
+        <p>The source for evidence-based information to nurture your body, mind and spirit.</p>
+        {!! Form::open(array('url' => '/logs','class'=>'form navbar-form navbar-left searchform')) !!}
+          {!! Form::text('search', null,
+                           array('required',
+                                'class'=>'form-control search',
+                                'placeholder'=>'Search for articles...')) !!}
+          {!!Form::submit('Search &raquo;', array('class' => 'btn btn-default btn-success search'))!!}
         {!! Form::close() !!}
       </div>
     </div>
-
 
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
         <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <h2>Body</h2>
+          <p>Healthy eating. Maintain a healthy environment. Stretching. Breathing. Working out. Yoga. Massage.  Sleeping. Resting. Sex. Cosmetics. </p>
+          <p><a class="btn btn-default" href="#" role="button">View articles &raquo;</a></p>
         </div>
         <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <h2>Mind</h2>
+          <p>Learning new things. Concentration and focus. Board games. Daydreaming. Reading. Doing new things. Writing. Crosswords, logic games and puzzles. </p>
+          <p><a class="btn btn-default" href="#" role="button">View articles &raquo;</a></p>
        </div>
         <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+          <h2>Spirit</h2>
+          <p>Meditation.  Purpose. Awareness. Gratitude. Music. Gardening. Letting go. Friends and family. Social networks. Hobbies. Volunteer. </p>
+          <p><a class="btn btn-default" href="#" role="button">View articles &raquo;</a></p>
         </div>
       </div>
 
-
-
-
       <hr>
 
-
-
-
       <footer>
-        <p>&copy; Company 2014</p>
+        <p>&copy; Zudbu 2015</p>
       </footer>
     </div> <!-- /container -->
-
-
-
 
 
     <!-- Bootstrap core JavaScript
