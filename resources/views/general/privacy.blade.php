1d9e7829a17aa6bbb6344a12916aa29b6ccc9779
Gerardo Castaneda <gerardocastaneda@g.harvard.edu>
2015-12-21 15:03:51 -0600

    Markup Validation

--- resources/views/general/privacy.blade.php
+++ resources/views/general/privacy.blade.php
@@ -10,21 +10,21 @@
 @section('body')
   <p>
     To achieve our mission of increasing wellness, we need to collect
-    a small set of information including:
+    a small set of information including:</p>
     <ul>
       <li>User information provided during registration</li>
       <li>Posted comments and user</li>
       <li>IP addresses of anonymous comments</li>
       <li>Article ownership</li>
     </ul>
-  </p>
+
   <p>
     We limit our communications to the essentials needed to provide our
-    services
+    services</p>
     <ul>
       <li>Confirming registration</li>
       <li>Confirm and answer contact requests</li>
       <li>Send emails for password resets</li>
     </ul>
-  </p>
+  
 @stop
